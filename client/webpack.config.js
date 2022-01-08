1eef622a35af085043cc599d2245ac3b1f06c9c8
Nicoletr <nicole.trpevski@gmail.com>
2022-01-08 16:29:13 +0800

    Updated manifest in service worker to reflect correct output

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,13 +32,14 @@ module.exports = () => {
 
       new GenerateSW(),
       new WebpackPwaManifest({
-        name: "JATE",
-        short_name: "JATE",
-        description: "Text editor",
-        background_color: "#7eb4e2",
-        theme_color: "#7eb4e2",
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E.",
+        description: "Takes notes with JavaScript syntax highlighting!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
         start_url: "/",
         publicPath: "/",
+        orientation: "portrait",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
